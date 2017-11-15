0372f734be0ed8433b94afe9465a06af08912469
Freek Paans <freekpaans@gmail.com>
2017-11-15 14:25:40 +0100

    lein new clojure-netty-proxy

---
+++ test/clojure_netty_proxy/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-netty-proxy.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-netty-proxy.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
